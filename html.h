ebb49d33f3cc0338d4d61c38b86caca6818505cd
Jonas Souza <jonassouza871@hotmail.com>
2025-07-21 00:23:49 -0300

    Separando o html em arquivo separado

---
+++ html.h
@@ -0,0 +1,10 @@
+#ifndef HTML_H
+#define HTML_H
+
+extern const char HTML_BODY[];
+extern const char HTML_GRAFICOS[];
+extern const char HTML_ESTADOS[];
+extern const char HTML_LIMITES[];
+extern const char HTML_CALIBRACAO[];
+
+#endif
